5556bd6071cd704b152e3b8ee42b0997d8d814e3
Leo Cheng <71459641+lenyung09@users.noreply.github.com>
2023-05-07 21:34:43 -0400

    More Scene Changes, With Water Sounds.

--- Assets/_Labs/07-Prototype3/Scripts/TriggerSFX.cs
+++ Assets/_Labs/07-Prototype3/Scripts/TriggerSFX.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class TriggerSFX : MonoBehaviour
 {
     public AudioSource playSound;
+    public GameObject fpsGuy;
 
     void OnTriggerEnter(Collider other)
     {
         playSound.Play();
+        fpsGuy.GetComponent<AudioSource>().enabled = !fpsGuy.GetComponent<AudioSource>().enabled;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        playSound.Pause();
+        fpsGuy.GetComponent<AudioSource>().enabled = !fpsGuy.GetComponent<AudioSource>().enabled;
     }
 }
